3015a07cae980888ddd3cc5b898e7193c7c84310
nashaofu <diaocheng@outlook.com>
2020-12-16 17:14:00 +0800

    add 添加cookie是否过期检测

--- src/v2ex.js
+++ src/v2ex.js
@@ -1,8 +1,12 @@
 const { chromium } = require('playwright')
 
-;(async () => {
+const url = 'https://v2ex.com/mission/daily'
+
+async function main() {
   const browser = await chromium.launch()
+
   const context = await browser.newContext()
+
   context.addCookies([
     {
       name: 'A2',
@@ -12,7 +16,21 @@ const { chromium } = require('playwright')
       httpOnly: true
     }
   ])
+
   const page = await context.newPage()
-  await page.goto('https://v2ex.com/')
+  await page.goto(url)
+  // 检测 Cookie 是否有效
+  if (url !== page.url()) {
+    throw new Error('页面 Cookie 过期')
+  }
   await browser.close()
-})()
+}
+
+main()
+  .then(() => {
+    console.log('任务执行完成')
+  })
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
