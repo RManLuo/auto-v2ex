1861a6616280623caf3a438dcbeacb92aa21bcb3
nashaofu <diaocheng@outlook.com>
2020-12-18 14:21:59 +0800

    feat v2ex完全支持

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # auto
-利用GitHub actions自动执行任务
+
+利用 GitHub actions 自动执行任务
+
+## v2ex
+
+支持 v2ex 每日自动领取奖励。
+
+### 部署步骤
+
+1. 登录 v2ex，打开浏览器控制台，进入 cookie 查看页面。![v2ex-cookie](./images/v2ex-cookie.jpg)
+2. 在 Github 设置中添加 secrets，secrets 名称为 `V2EX_A2`，value 为 v2ex 网站 cookie 中的`A2`。![v2ex-secrets](./images/v2ex-secrets.jpg)
--- src/v2ex.js
+++ src/v2ex.js
@@ -1,6 +1,6 @@
 const { chromium } = require('playwright')
 
-const url = 'https://v2ex.com/mission/daily'
+const url = 'https://v2ex.com'
 
 async function main() {
   const browser = await chromium.launch()
@@ -10,7 +10,7 @@ async function main() {
   context.addCookies([
     {
       name: 'A2',
-      value: process.env.A2,
+      value: process.env.V2EX_A2,
       domain: '.v2ex.com',
       path: '/',
       httpOnly: true
@@ -19,10 +19,15 @@ async function main() {
 
   const page = await context.newPage()
   await page.goto(url)
-  // 检测 Cookie 是否有效
-  if (url !== page.url()) {
-    throw new Error('页面 Cookie 过期')
-  }
+
+  // 点击 领取今日的登录奖励 链接
+  // 必须经过这个页面过去，否则领取会异常
+  await page.click('a[href="/mission/daily"]')
+
+  // 点击领取按钮
+  await page.click('#Main > div.box > div:nth-child(2) > input')
+
+  // 关闭页面
   await browser.close()
 }
 
